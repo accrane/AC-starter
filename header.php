76a96c5bda70ec4b485b9cf391b01a3ecbe97bf7
Austin Crane <austincrane@gmail.com>
2017-12-24 10:57:13 -0500

    new commit

---
+++ header.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The header for theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package ACStarter
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'acstarter' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="wrapper">
+			
+			<?php if(is_home()) { ?>
+	            <h1 class="logo">
+	            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+	            </h1>
+	        <?php } else { ?>
+	            <div class="logo">
+	            <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>
+	            </div>
+	        <?php } ?>
+
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'acstarter' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
+	</div><!-- wrapper -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content wrapper">
---
+++ inc/post-types.php
@@ -0,0 +1,45 @@
+<?php 
+/* Custom Post Types */
+
+add_action('init', 'js_custom_init');
+function js_custom_init() 
+{
+	
+	// Register the Homepage Slides
+  
+     $labels = array(
+	'name' => _x('Slides', 'post type general name'),
+    'singular_name' => _x('Slide', 'post type singular name'),
+    'add_new' => _x('Add New', 'Slide'),
+    'add_new_item' => __('Add New Slide'),
+    'edit_item' => __('Edit Slides'),
+    'new_item' => __('New Slide'),
+    'view_item' => __('View Slides'),
+    'search_items' => __('Search Slides'),
+    'not_found' =>  __('No Slides found'),
+    'not_found_in_trash' => __('No Slides found in Trash'), 
+    'parent_item_colon' => '',
+    'menu_name' => 'Slides'
+  );
+  $args = array(
+	'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'show_in_menu' => true, 
+    'query_var' => true,
+    'rewrite' => true,
+    'capability_type' => 'post',
+    'has_archive' => false, 
+    'hierarchical' => false, // 'false' acts like posts 'true' acts like pages
+    'menu_position' => 20,
+    'supports' => array('title','editor','custom-fields','thumbnail'),
+	
+  ); 
+  register_post_type('slide',$args); // name used in query
+  
+  // Add more between here
+  
+  // and here
+  
+  } // close custom post type

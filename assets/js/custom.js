022d5607f9b0592554ac3101a7ab8186c1f2d9f4
Austin Crane <austincrane@gmail.com>
2018-02-01 13:01:04 -0500

    better nav starter

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,6 +17,19 @@ jQuery(document).ready(function ($) {
         $(this).addClass("active");
         }
 	});
+
+	/*
+	*
+	*	Responsive iFrames
+	*
+	------------------------------------*/
+	var $all_oembed_videos = $("iframe[src*='youtube']");
+	
+	$all_oembed_videos.each(function() {
+	
+		$(this).removeAttr('height').removeAttr('width').wrap( "<div class='embed-container'></div>" );
+ 	
+ 	});
 	
 	/*
 	*
